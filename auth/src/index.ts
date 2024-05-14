777e83a319140dcd47c4e2b70b1ebb9fde20644d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-14 04:08:34 +0300

    initial commit

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,48 @@
-import express from 'express'
-import 'express-async-errors'
-import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-import { signupRouter } from './routes/signup'
-import { errorHandler } from './middleware/error-handler'
-const PORT = 3000
+import express from 'express';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middleware/error-handler';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { NotFoundError } from './errors/not-found';
 
-const app = express()
+const PORT = 3000;
 
-app.use(express.json())
+const app = express();
 
-app.set('truest proxy', true)
+app.use(express.json());
 
-app.use(cookieSession({
-  signed: false,
-  secure: false
-}))
+app.set('truest proxy', true);
 
-app.use(signupRouter)
+app.use(
+  cookieSession({
+    signed: false,
+    secure: false,
+  })
+);
+
+app.use(signupRouter);
+
+app.use(signinRouter);
+
+app.use(signoutRouter);
 
 app.all('*', () => {
-  
-})
+  throw new NotFoundError();
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-
-const start  = async() => {
-  await mongoose.connect('mongodb://127.0.0.1:27017/tiketi6-auth?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1')
+const start = async () => {
+  await mongoose.connect(
+    'mongodb://127.0.0.1:27017/tiketi6-auth?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1'
+  );
 
   app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-  })
-}
+    console.log(`server running on port ${PORT}`);
+  });
+};
 
-start()
+start();
